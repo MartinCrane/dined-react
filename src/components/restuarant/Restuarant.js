43a4c96472865ef97d1dd42aa506a9a231bb9569
MartinCrane <martin.crane@gmail.com>
2017-04-17 17:26:22 -0400

    basic components

---
+++ src/components/restuarant/Restuarant.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export class Restuarant extends Component {
+  render(){
+    return(
+        <div className="panel panel-default">
+          <h1>Name </h1>
+          <h1>Price </h1>
+        </div>
+    )
+  }
+}
