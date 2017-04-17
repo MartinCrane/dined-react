72fdc0115a50585f94f131c051beb09c420d8118
MartinCrane <martin.crane@gmail.com>
2017-04-17 18:01:48 -0400

    more components

---
+++ src/components/sessions/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export class Login extends Component {
+  render(){
+    return(
+        <div className="login">
+          <h1>email</h1>
+          <h1>password </h1>
+        </div>
+    )
+  }
+}
