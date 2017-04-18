672a67f4a8e94b5f5e226dc7efda6d080b6609b1
gee2789 <geeyung.li@gmail.com>
2017-04-18 15:42:47 -0400

    fixed mapDispatch and mapProps

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ import './index.css';
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
-const store = createStore(rootReducer, applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
- )
+const store = createStore(rootReducer, applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
   <Provider store={store} >
--- src/reducers/accountReducer.js
+++ src/reducers/accountReducer.js
@@ -1,5 +1,4 @@
 export default (state={login: false}, action) => {
-  debugger
   switch (action.type) {
     case "SET_LOGIN":
       return {login: action.payload}
