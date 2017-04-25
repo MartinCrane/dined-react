97dfdc9ecde0b1c66c97b50a4fa1c56ea8758ae4
MartinCrane <martin.crane@gmail.com>
2017-04-24 21:25:45 -0400

    new buttons

--- src/components/panels/NavBarStorage.js
+++ src/components/panels/NavBarStorage.js
@@ -41,3 +41,65 @@ const horizontal = <div>
                       onClick={this.handleClick.bind(null, "search")}>Search</Button>
                     {this.props.login ? <Button onClick={(event) => this.logout(event)}>Logout</Button> : null}
                   </ButtonToolbar>
+
+                  const sideBar = (
+                                      <ButtonGroup vertical block style={{left: this.state.offset, borderRadius: '0px'}}>
+                                          <Button key={1} className="verticalNavButton"
+                                                  onClick={this.expandMenu}>
+                                            <Row>
+                                              <Col xs ={3} sm={3} md={3}>PICKY</Col>
+                                              <Col xs={7} sm={7} md={7}></Col>
+                                            </Row>
+                                          </Button>
+                                          <Button key={2} className="verticalNavButton"
+                                                  onClick={this.handleClick.bind(null, "Map")}>
+                                            <Row>
+                                              <Col xs={3} sm={3} md={3}>Maps</Col>
+                                              <Col xs={7} sm={7} md={7}></Col>
+                                              <Col xs={1} sm={1} md={1}>
+                                                <img style={{align: 'right', height: '25px'}}src={require('../../images/maps.png')}></img>
+                                              </Col>
+                                            </Row>
+                                          </Button>
+                                          <Button key={3} className="verticalNavButton"
+                                                  onClick={this.handleClick.bind(null, "Search")}>
+                                            <Row>
+                                              <Col xs={3} sm={3} md={3}>Search</Col>
+                                              <Col xs={7} sm={7} md={7}></Col>
+                                              <Col xs={1} sm={1} md={1}>
+                                                <img style={{align: 'right', height: '25px'}}src={require('../../images/search.png')}></img>
+                                              </Col>
+                                            </Row>
+                                          </Button>
+                                          <Button key={4} className="verticalNavButton"
+                                                  onClick={this.handleClick.bind(null, "Favorites")}>
+                                            <Row>
+                                              <Col xs={3} sm={3} md={3}>Favorites</Col>
+                                              <Col xs={7} sm={7} md={7}></Col>
+                                              <Col xs={1} sm={1} md={1}>
+                                                <img style={{align: 'right', height: '25px'}}src={require('../../images/fav.png')}></img>
+                                              </Col>
+                                            </Row>
+                                          </Button>
+                                          <Button key={5} className="verticalNavButton"
+                                                  onClick={this.handleClick.bind(null, "Roulette")}>
+                                            <Row>
+                                              <Col xs={3} sm={3} md={3}>Roulette</Col>
+                                              <Col xs={7} sm={7} md={7}></Col>
+                                              <Col xs={1} sm={1} md={1}>
+                                                <img style={{align: 'right', height: '25px'}}src={require('../../images/roulette.png')}></img>
+                                              </Col>
+                                            </Row>
+                                          </Button>
+                                          <Button key={6} className="verticalNavButton"
+                                                  onClick={this.logout}>
+                                            <Row>
+                                              <Col xs={3} sm={3} md={3}>Lougout</Col>
+                                              <Col xs={7} sm={7} md={7}></Col>
+                                              <Col xs={1} sm={1} md={1}>
+                                                <img style={{align: 'right', height: '25px'}}src={require('../../images/logout.png')}></img>
+                                              </Col>
+                                            </Row>
+                                          </Button>
+                                    </ButtonGroup>
+                                    );
